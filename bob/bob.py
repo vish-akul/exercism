4de6d1af2c50a0d9eba0efaa5eb960a2c604cbaf
Akul Pillai <vishnu@akulpillai.com>
2017-08-09 21:17:15 +0530

    Solved a Problem, Handled Execeptions of previous one.

--- bob/bob.py
+++ bob/bob.py
@@ -1,13 +1,19 @@
 import re
 
-def hey(statement):
-    statement = statement.strip()
-    if statement == '':
-        return 'Fine. Be that way!'
-    elif statement.isupper():
-        return 'Whoa, chill out!'
-    elif '?' == statement[-1]:
-        return 'Sure.'
-    elif not re.search('[a-zA-Z0-9]+',statement):
-        return 'Fine. Be that way!'
-    return 'Whatever.'
+def hey(original_statement):
+
+    bob_reply = ''
+    statement = original_statement.strip()
+
+    if statement == '' :
+        bob_reply = 'Fine. Be that way!'
+    elif statement.isupper() == True :
+        bob_reply = 'Whoa, chill out!'
+    elif statement[-1] == '?' :
+        bob_reply = 'Sure.'
+    elif re.search('[a-zA-Z0-9]+',statement) is not None:
+        bob_reply =  'Fine. Be that way!'
+    else:
+        bob_reply = 'Whatever.'
+
+    return bob_reply
--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,37 @@
-def decode():
-    pass
+def decode(coded):
+    letter = ''
+    number = ''
+    num1 = 0
+    ucoded = ''
+    if coded.isalpha():
+        return coded
+    for i in range(0,len(coded)):
+        if coded[i].isalpha() or coded[i] == ' ':
+            nletter = coded[i]
+        elif coded[i+1].isalpha() or coded[i+1] == ' ':
+            nletter = coded[i+1]
+        if coded[i].isdigit():
+            number += coded[i]
+            if coded[i+1].isalpha():
+                num1 = int(number)
+                ucoded += nletter*num1
+                number = ''
+    return ucoded
 
 
-def encode():
-    pass
+
+def encode(ucoded):
+    count = 1
+    var1 = ''
+    coded = ''
+    for i in range(0,len(ucoded)):
+        var1 = ucoded[i]
+        if (i+1) != len(ucoded) and ucoded[i+1] == var1:
+            count += 1
+        else:
+            if count == 1:
+                coded += var1
+            else:
+                coded += str(count)+var1
+                count = 1
+    return coded
